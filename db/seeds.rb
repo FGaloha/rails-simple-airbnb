8d1df6ef5b76b26c5f541041d33c3f7c76f4c3f0
Flavia Gallois <fgbis@hotmail.com>
2021-11-10 19:02:38 +0100

    exo 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+count = 0
+
+5.times do
+  name = "Flat #{count}"
+  address = "#{count} avenue de Paris 75001 Paris"
+  flat = Flat.new(
+    name: name,
+    description: "Superbe appartement plein ouest",
+    address: address,
+    price_per_night: 123,
+    number_of_guests: 2
+  )
+  flat.save
+  count += 1
+end
